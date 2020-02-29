656449e14fe636f950012d9ecd280d2d4770fb85
DAMICHOI <choidami93@gmail.com>
2020-02-29 16:06:08 +0900

    3주차 수업_2020.02.29

--- scoreboard/src/components/Header.js
+++ scoreboard/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Statistics} from "./Statistics";  // JSX를 쓰기 때문에 Webstorm이 자동으로 추가함
+import {Stopwatch} from "./Stopwatch";
 
 // Named export
 export const Header = (props) => {
@@ -8,6 +9,7 @@ export const Header = (props) => {
             <Statistics players={props.players}/>
             <h1 className="h1">{props.title}</h1>
             <span className="stats">Players: {props.totalPlayers}</span>
+            <Stopwatch></Stopwatch>
         </header>
     );
 }

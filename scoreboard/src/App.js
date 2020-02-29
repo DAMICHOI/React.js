4d62afe80aeb83d5753d2ab0f276b1a18f419a0d
DAMICHOI <choidami93@gmail.com>
2020-02-29 10:10:02 +0900

    3주차 수업_2020.02.29

--- scoreboard/src/App.js
+++ scoreboard/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; // global로 정의된다.
 
+/*
+// 2020.02.22
 function App() {
   return (
     <div className="App">
@@ -21,6 +22,89 @@ function App() {
       </header>
     </div>
   );
+}
+*/
+
+// 2020.02.29 Migration
+const Header = (props) => {
+  return (
+      <header className="header">
+        <h1 className="h1">{props.title}</h1>
+        <span className="stats">Players: {props.totalPlayers}</span>
+      </header>
+  );
+}
+
+
+const Player = (props) => {
+  return (
+      <div className="player">
+            <span className="player-name">
+                <button className="remove-player" onClick={() => props.removePlayer(props.id)}> x </button>
+              {props.name}
+            </span>
+        <Counter score={props.score}/>
+      </div>
+  );
+}
+
+class Counter extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      score: 0
+    }
+  }
+
+  handleChange = (delta) => {
+    console.log(this);
+    this.setState(prevState => ({score: prevState.score + delta}));
+  }
+
+  render() {
+    return (
+        <div className="counter">
+          <button className="counter-action decrement" onClick={() => this.handleChange(-1)}> - </button>
+          <span className="counter-score">{this.state.score}</span>
+          <button className="counter-action increment" onClick={() => this.handleChange(1)}> + </button>
+        </div>
+    );
+  }
+}
+
+class App extends React.Component{
+  state = {
+    players: [
+      {name: 'LDK', id: 1},
+      {name: 'HONG', id: 2},
+      {name: 'KIM', id: 3},
+      {name: 'PARK', id: 4}
+    ]
+  }
+
+  handleRemove = (id) => {
+    console.log('handleRemove', id)
+    this.setState(prevState => {
+      const players = prevState.players.filter(player => { return player.id !== id });
+      return {players};
+    })
+  }
+
+  render() {
+    return (
+        <div className="scoreboard">
+          <Header title="My Scoreboard" totalPlayers={this.state.players.length}/>
+          {
+            this.state.players.map(player => (
+                <Player name={player.name} score={player.score} id={player.id} key={player.id}
+                        removePlayer={this.handleRemove}/>
+            ))
+          }
+        </div>
+    );
+  }
 }
 
 export default App;

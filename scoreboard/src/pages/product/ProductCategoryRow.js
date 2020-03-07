88bc5585dc2b6b8413d20177f9bde15cb9b57bc6
DAMICHOI <choidami93@gmail.com>
2020-03-07 16:01:56 +0900

    4주차 수업_2020.03.07 - product 메뉴 구현 (검색기능 구현)

--- scoreboard/src/pages/product/ProductCategoryRow.js
+++ scoreboard/src/pages/product/ProductCategoryRow.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const ProductCategoryRow = (props) => {
     return (
-        <tr colspan="2">
+        <tr colSpan="2">
             <th>{props.category}</th>
         </tr>
     );
--- scoreboard/src/pages/product/SearchBar.js
+++ scoreboard/src/pages/product/SearchBar.js
@@ -3,7 +3,9 @@ import React, {Fragment} from 'react';
 export const SearchBar = (props) => {
     return (
         <>
-            <input placeholder="Search..."></input>
+            <input placeholder="Search..."
+                   value={props.keyword}
+                   onChange={(e) => props.setKeyword(e.target.value)}></input>
             <br/>
             <input type="checkbox"></input> Only Show products in stock
         </>

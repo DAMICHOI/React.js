41102689f4065ffae6b1f478b918f4464cac449a
DAMICHOI <choidami93@gmail.com>
2020-03-07 16:48:28 +0900

    4주차 수업_2020.03.07 - 기본 라우팅 설정

--- scoreboard/src/index.js
+++ scoreboard/src/index.js
@@ -6,9 +6,11 @@ import * as serviceWorker from './serviceWorker';
 import {store} from "./redux/store";
 import {Provider} from "react-redux";
 import {FilterableProductTable} from "./pages/product/FilterableProductTable";
+import {Root} from "./pages/Root";
 
-ReactDOM.render(<Provider store={store}><FilterableProductTable/></Provider>, document.getElementById('root'));
 //ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
+//ReactDOM.render(<Provider store={store}><FilterableProductTable/></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><Root/></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
